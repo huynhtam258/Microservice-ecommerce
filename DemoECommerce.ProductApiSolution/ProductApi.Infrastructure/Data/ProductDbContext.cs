4c0bb051b6bcc9bad05375f85f9e44b813e4b56d
liam nguyen <long.ndh@nscsoftware.com>
2026-07-18 17:50:53 +0700

    update product db context

--- DemoECommerce.ProductApiSolution/ProductApi.Infrastructure/Data/ProductDbContext.cs
+++ DemoECommerce.ProductApiSolution/ProductApi.Infrastructure/Data/ProductDbContext.cs
@@ -3,7 +3,7 @@ using ProductApi.Domain.Entities;
 
 namespace ProductApi.Infrastructure.Data
 {
-    public class ProductDbContext(DbContextOptions<ProductDbContext> options):DbContext
+    public class ProductDbContext(DbContextOptions<ProductDbContext> options) : DbContext(options)
     {
         public DbSet<Product> Products { get; set; }
     }
